3651aeebbf7b4448ecc79d1c3f63c2c4f077c822
Boka <Maxutca156@yandex.ru>
2024-02-05 17:45:28 +0300

    gg

---
+++ homework.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_into_by_ip(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        print(response)
+
+        data = {
+            '[IP]': response.get('query'),
+            '[Int prov]': response.get('isp'),
+            '[Org]': response.get('org'),
+            '[Country]': response.get('query'),
+        }
+
+    except requests.exceptions.ConnectionError:
+        print('[!] Please check your connection!')
+
